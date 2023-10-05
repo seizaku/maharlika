26bf408176562c6e4c80fb7cb0ee12b0d8b2dc2c
Zakusei <zakusei.dev@gmail.com>
2023-10-05 18:48:00 +0800

    feat(navbar): add navbar

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "@/components/Navbar";
+import { Hero } from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between px-44 pt-20"></main>
+    </>
+  );
+}
