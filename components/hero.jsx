c663b03fee3f55ff7750f791ac9425936b833f30
Zakusei <zakusei.dev@gmail.com>
2023-10-08 00:17:24 +0800

    feat(hero): add hero component

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,8 +1,9 @@
 import HeroImage from "@/public/assets/images/hero5.jpg";
+import Clouds from "@/components/clouds";
 
 export const Hero = () => {
   return (
-    <section className="relative flex min-h-screen w-screen items-center justify-center">
+    <section className="relative flex h-screen w-screen items-center justify-center">
       <div
         className="bg top-0 h-full w-full"
         style={{
@@ -15,10 +16,10 @@ export const Hero = () => {
       />
       <div className="absolute top-0 z-10 h-full w-full bg-background/80" />
       <div className="absolute z-20 text-center">
-        <h1 className="text-3xl font-semibold tracking-widest text-foreground sm:text-5xl md:text-7xl">
+        <h1 className="text-3xl font-semibold tracking-widest text-foreground drop-shadow-xl sm:text-5xl md:text-7xl">
           MAHARLIKA
         </h1>
-        <p className="text-xs leading-7 text-foreground md:px-[25%] md:text-xl">
+        <p className="text-xs font-medium leading-7 text-foreground drop-shadow-xl md:px-[25%] md:text-xl">
           PHILIPPINES
         </p>
       </div>
