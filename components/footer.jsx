a0abf3f73f97ad52625d28bf84783511e016f055
Zakusei <zakusei.dev@gmail.com>
2023-10-08 00:17:49 +0800

    feat(footer): add footer component

---
+++ components/footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="flex h-20 w-full items-center justify-center border-t-input bg-neutral-200 px-2 text-center dark:bg-neutral-900 md:px-6 lg:top-0 xl:px-44">
+      © Zakusei 2023. All Rights Reserved.
+    </footer>
+  );
+};
